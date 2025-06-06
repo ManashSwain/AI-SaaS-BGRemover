d2b10a7c54fd9b85e810650d8865a9b16797722f
ManashSwain <manash0802@gmail.com>
2025-06-06 18:29:17 +0530

    implement: layout structure

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
 <>
+<Navbar/>
+<Outlet/>
+<Footer/>
 
 </>
   )
