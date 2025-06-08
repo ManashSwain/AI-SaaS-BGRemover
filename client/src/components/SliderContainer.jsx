7a6d58cce791b6058418d11c8d1e0a12aac3098a
ManashSwain <manash0802@gmail.com>
2025-06-08 10:08:11 +0530

    implement: ui changes in slider container

--- client/src/components/SliderContainer.jsx
+++ client/src/components/SliderContainer.jsx
@@ -5,6 +5,9 @@ const SliderContainer = () => {
   return (
     <>
     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 border-gray-900 bg-red-600">
+        <h1 className="mb-8 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
+            Remove Background With High Quality and Accuracy
+          </h1> 
         <Slider/>
     </div>
     </>
