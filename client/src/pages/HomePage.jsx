1769d1f6f9e13a840b4f8aaf92ccf6d072c86af3
ManashSwain <manash0802@gmail.com>
2025-06-08 10:34:00 +0530

    implement: ui design for steps

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero'
 import SliderContainer from '../components/SliderContainer'
+import Steps from '../components/Steps'
 
 const HomePage = () => {
   return (
    <>
    <Hero/>
+   <Steps/>
    <SliderContainer/>
    </>
   )
