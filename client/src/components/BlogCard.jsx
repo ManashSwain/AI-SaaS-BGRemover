c4316a3bb1a770dc79dd1f8e2d54e9f775ff3673
ManashSwain <manash0802@gmail.com>
2025-06-08 23:11:56 +0530

    implement: ui changes in blog card

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -3,6 +3,16 @@ import React from "react";
 const BlogCard = () => {
   return (
     <>
+    <div className="bg-yellow-500 py-12 px-4">
+        <div className="flex justify-end">
+             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-700 md:text-5xl lg:text-6xl">
+            Blog
+          </h1>
+           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
+              See more articles
+          </p>
+        </div>
+        <div>
       <a
         href="#"
         className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
@@ -15,6 +25,8 @@ const BlogCard = () => {
           far, in reverse chronological order.
         </p>
       </a>
+      </div>
+      </div>
     </>
   );
 };
